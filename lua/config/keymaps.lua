040780b2b7894688193fb8b85ab0250eb9a3de72
henry <devgony@gmail.com>
2025-01-02 16:13:26 +0900

    Convert map to safe_del_keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,8 +8,34 @@ map("n", "<c-t>", function()
 end, { desc = "Terminal (Root Dir)" })
 map("t", "<C-t>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
-map({ "n", "i", "v" }, "<M-j>", "<nop>", { desc = "Disable <M-j>" })
-map({ "n", "i", "v" }, "<M-k>", "<nop>", { desc = "Disable <M-k>" })
+-- map({ "n", "i", "v" }, "<M-j>", "<nop>", { desc = "Disable <M-j>" })
+-- map({ "n", "i", "v" }, "<M-k>", "<nop>", { desc = "Disable <M-k>" })
+--
+-- map({ "n", "i", "v" }, "<M-p>", "<nop>", { desc = "Disable <M-p>" })
+-- map({ "n", "i", "v" }, "<M-n>", "<nop>", { desc = "Disable <M-n>" })
+
+local function safe_del_keymap(mode, lhs)
+  if vim.fn.maparg(lhs, mode) ~= "" then
+    vim.api.nvim_del_keymap(mode, lhs)
+  end
+end
+
+safe_del_keymap("n", "<M-j>")
+safe_del_keymap("i", "<M-j>")
+safe_del_keymap("v", "<M-j>")
+
+safe_del_keymap("n", "<M-k>")
+safe_del_keymap("i", "<M-k>")
+safe_del_keymap("v", "<M-k>")
+
+safe_del_keymap("n", "<M-p>")
+safe_del_keymap("i", "<M-p>")
+safe_del_keymap("v", "<M-p>")
+
+safe_del_keymap("n", "<M-n>")
+safe_del_keymap("i", "<M-n>")
+safe_del_keymap("v", "<M-n>")
+
 map({ "v" }, "J", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
 map({ "v" }, "K", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
