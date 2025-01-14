6775ff55a9354ff118973fa930126f338d01b4b6
devgony <devgony@gmail.com>
2025-01-14 21:29:38 +0900

    Set clipboard to sync with os

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,5 @@
 -- Add any additional options here
 local opt = vim.opt
 opt.shiftwidth = 4 -- Size of an --indent
+opt.clipboard = "unnamedplus"
+-- opt.langmap = "ㅁa,ㅠb,ㅊc,ㅇd,ㄷe,ㄹf,ㅎg,ㅗh,ㅑi,ㅓj,ㅏk,ㅣl,ㅡm,ㅜn,ㅐo,ㅔp,ㅂq,ㄱr,ㄴs,ㅅt,ㅕu,ㅍv,ㅈw,ㅌx,ㅛy,ㅋz"
