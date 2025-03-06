34de5178d830e664c71c3ab75c2776a1313f8ffd
henry <devgony@gmail.com>
2025-03-06 13:07:16 +0900

    use claude-3.7-thought

---
+++ lua/plugins/CopilotChat.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = function()
+      local user = vim.env.USER or "User"
+      user = user:sub(1, 1):upper() .. user:sub(2)
+      return {
+        model = "claude-3.7-sonnet-thought",
+        auto_insert_mode = true,
+        question_header = "  " .. user .. " ",
+        answer_header = "  Copilot ",
+        window = {
+          width = 0.4,
+        },
+      }
+    end,
+  },
+}
